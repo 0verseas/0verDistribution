ec189f78053c310d47b13e474f9acf611600caa5
Rick <ricklai82@gmail.com>
2021-03-30 09:47:19 +0800

    讓各校可以查看並登記學生資料。

---
+++ src/js/studentList.js
@@ -0,0 +1,276 @@
+(()=>{
+    const $paginationContainer = $('#pagination-container'); // 分頁區域
+    const $studentList = $('#student-list');
+
+    const $editStudentInfoModal = $('#editStudentInfoModal'); // 學生資料編輯區域
+    const $modalStudentInfo = $('#modal-studentInfo'); // 學生資料
+    const $year = $modalStudentInfo.find('#year');
+    const $schoolName = $modalStudentInfo.find('#schoolName');
+    const $name = $modalStudentInfo.find('#name');
+    const $overseasId = $modalStudentInfo.find('#overseasId');
+    const $department = $modalStudentInfo.find('#department');
+    const $chineseGrade = $modalStudentInfo.find('#chineseGrade');
+    const $distributeGrade = $modalStudentInfo.find('#distributeGrade');
+    const $distributeDate = $modalStudentInfo.find('#distributeDate');
+    const $distributeNumber = $modalStudentInfo.find('#distributeNumber');
+    const $confirmStatus = $modalStudentInfo.find('#confirmStatus');
+    const $studentStatus = $modalStudentInfo.find('#studentStatus');
+    const $studentStatusReason = $modalStudentInfo.find('#studentStatusReason');
+    const $statusDate = $modalStudentInfo.find('#statusDate');
+    const $registerDate = $modalStudentInfo.find('#registerDate');
+
+    const $schoolYear = $modalStudentInfo.find('#schoolYear');
+    const $suspension = $modalStudentInfo.find('#suspension');
+    const $academicCredit = $modalStudentInfo.find('#academicCredit');
+    const $averageGrades = $modalStudentInfo.find('#averageGrades');
+    const $classPopulation = $modalStudentInfo.find('#classPopulation');
+    const $ranking = $modalStudentInfo.find('#ranking');
+    const $rankingOfPopulation = $modalStudentInfo.find('#rankingOfPopulation');
+
+    const $saveBtn = $('#btn-studentInfo-save');
+    const $uploadBtn = $('#student-list-upload-button');
+    const $fileInput = $('#file-input');
+
+    /**
+     * bind event
+     */
+    $saveBtn.on('click', _handleSave);
+    $uploadBtn.on('click', _uploadStudentList);
+    $fileInput.on('change', _handleFileInput);
+
+    /**
+     * init
+     */
+    _setData();
+
+    function _setData() {
+        // 設定下載學生名冊按鈕事件
+        $('#student-list-download-button').attr('href', env.baseUrl + '/editors/indonesia/create');
+
+        loading.start();
+
+        //取得資料渲染學生列表
+        User.getStudentList().then(function (res) {
+            if (res.ok) {
+                // loading.start();
+                return res.json();
+            } else {
+                throw res.status;
+            }
+        }).then(function (json) {
+            // console.log(json);
+            $studentAllList=json;
+            $paginationContainer.pagination({
+                dataSource: json,
+                pageSize: 20,
+                callback: function(json, pagination) {
+                    _studentListTamplate(json,pagination.pageNumber);
+                    $editStudentInfoBtn = $('.btn-editStudentInfo'); // 新增學生資料編輯按鈕的觸發事件（開啟 Modal）
+                    $editStudentInfoBtn.on('click', _handleEditStudentInfo);
+                }
+            });
+        }).then(function (){
+            loading.complete();
+        }).catch(function (err) {
+            loading.complete();
+            if (err == 401) {
+                //alert("權限不足");
+                window.location.href = './login.html';
+            }
+        });   
+    }
+
+    function _studentListTamplate(json,page){
+        // 渲染 student 列表
+        $studentList.html('');
+        json.forEach(function (data, index) {
+            let studentHtml =`<tr class="btn-editStudentInfo" data-overseasid="${data[2]}" data-name="${data[3]}"><td>${index+1+((page-1)*20)}</td>`;
+            data.forEach(function (value, index) {
+                if(index == 4 || index == 6 || index == 7){
+                    
+                } else {
+                    studentHtml+=`<td>${(value)}</td>`
+                }
+            });
+            studentHtml+=`</tr>`;
+            $studentList.append(studentHtml);
+        });
+    }
+
+    async function _handleEditStudentInfo() { // 學生列表 Modal 觸發
+        $editStudentInfoModal.modal({backdrop: true, keyboard:true,}); // 讓使用者 點選外圍或按下esc時關閉Modal
+        _currentOverseasId = $(this).data('overseasid');
+        _currentName = $(this).data('name');
+
+        User.getStudentInfo({'overseasId':_currentOverseasId, 'name':_currentName})
+        .then((res) => {
+            if(res.ok){
+                return res.json();
+            } else {
+                throw res;
+            }
+        })
+        .then((json) => {
+            $year.val(json[0]);
+            $schoolName.val(json[3]);
+            
+            $name.val(json[2]);
+            $overseasId.val(json[1]);
+
+            $department.val(json[4]);
+
+            $distributeDate.val(json[7]);
+            $distributeNumber.val(json[8]);
+
+            $confirmStatus.val(json[9]);
+            $studentStatus.val(json[10]);
+
+            $studentStatusReason.val(json[11]);
+
+            $statusDate.val(json[12]);
+            $registerDate.val(json[13]);
+
+            $schoolYear.val(json[14]);
+            $suspension.val(json[15]);
+
+            $academicCredit.val(json[16]);
+            $averageGrades.val(json[17]);
+
+            $classPopulation.val(json[18]);
+            $ranking.val(json[19]);
+            $rankingOfPopulation.val(json[20]);
+        })
+        .then(()=>{
+            $editStudentInfoModal.modal({
+                backdrop: 'static',
+                keyboard: false
+            });
+        })
+        .catch((err) => {
+            err.json && err.json().then((data) => {
+                console.error(data);
+            })
+        })
+    }
+
+    // 單一學生資料修改並儲存
+    function _handleSave(){
+        // 準備好要傳遞的資料
+        let data;
+        data = {
+            'name':document.getElementById('name').value,
+            'overseasId':document.getElementById('overseasId').value,
+            'schoolYear':document.getElementById('schoolYear').value,
+            'suspension':document.getElementById('suspension').value,
+            'academicCredit':document.getElementById('academicCredit').value,
+            'averageGrades':document.getElementById('averageGrades').value,
+            'classPopulation':document.getElementById('classPopulation').value,
+            'ranking':document.getElementById('ranking').value,
+            'rankingOfPopulation':document.getElementById('rankingOfPopulation').value
+        };
+        
+        // 使用者確認
+        swal({
+            title: '確認是否要儲存並覆蓋學生資料',
+            html: `更動後資料：<br/>年級：${data.schoolYear}<br/>
+            是否曾經休學(以學期計)：${data.suspension}<br/>
+            累計實得學分數：${data.academicCredit}<br/>
+            學業成績總平均：${data.averageGrades}<br/>
+            全班(組)人數：${data.classPopulation}<br/>
+            名次：${data.ranking}<br/>
+            名次佔全班(組)百分比：${data.rankingOfPopulation}`,
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: '儲存',
+            cancelButtonText: '取消',
+            confirmButtonClass: 'btn btn-success',
+            cancelButtonClass: 'btn btn-danger',
+            buttonsStyling: false
+        }).then(()=>{
+            _saveEvent(data)
+        }).catch(()=>{
+        });
+    }
+
+    // 發送欲修改資料跟請求到後端
+    function _saveEvent(data){
+        User.saveStudentInfo(data)
+        .then((res) => {
+            if(res.ok){
+                alert('儲存成功');
+                return ;
+            } else {
+                throw res;
+            }
+        })
+        .catch((err) => {
+            err.json && err.json().then(() => {
+                alert('儲存失敗');
+            })
+        })
+    }
+
+    function _uploadStudentList(){
+        $fileInput.trigger('click');
+    }
+    
+    function _handleFileInput(){
+        const file = this.files[0];
+        $fileInput.val('');
+        if (file.name.split('.').pop() !== 'csv') {
+			alert('請匯入 .csv 檔');
+        }
+
+        // 需先讀成 binary string 以判斷編碼
+		const fileReaderAsBinaryString = new FileReader();
+		const fileReaderAsText = new FileReader();
+
+		fileReaderAsBinaryString.onload = function (e) {
+			// 偵測檔案編碼
+			const encoding = window.jschardet.detect(e.target.result).encoding;
+			// 使用偵測的編碼來讀取檔案成文字
+			fileReaderAsText.readAsText(file, encoding);
+		};
+
+		fileReaderAsText.onload = function (e) {
+			_renderCSVTable(e.target.result);
+		};
+
+		// 讀入檔案判斷編碼
+		fileReaderAsBinaryString.readAsBinaryString(file);
+    }
+
+    function _renderCSVTable(data) {
+		const csv_datas = window.User.CSVToArray(data);
+		const header = csv_datas.shift();
+        csv_datas.pop();
+		const fieldLength = header.length;
+		if (fieldLength !== 20) {
+			alert ('匯入之 csv 欄位數量有誤');
+			return;
+		}
+        loading.start();
+        User.uploadStudentList(csv_datas)
+        .then((res) => {
+            if(res.ok){
+                return res.json();
+            } else {
+                throw res;
+            }
+        })
+        .then(()=>{
+            alert('匯入成功');
+            window.location.reload();
+            loading.complete();
+        })
+        .catch((err) => {
+            err.json && err.json().then((data) => {
+                console.error(data);
+                alert('匯入失敗')
+            })
+            loading.complete();
+        });
+    }
+})();
--- src/js/user.js
+++ src/js/user.js
@@ -157,6 +157,94 @@ var User = (function () {
         });
     }
 
+    function  getStudentList() {
+        return fetch(baseUrl + '/editors/indonesia', {
+            method: 'GET',
+			credentials: 'include'
+        });
+    }
+
+    function  getStudentInfo(studentInfo) {
+        return fetch(baseUrl + `/editors/indonesia/${studentInfo.name}/${studentInfo.overseasId}`, {
+            method: 'GET',
+			credentials: 'include'
+        });
+    }
+
+    function  saveStudentInfo(data) {
+        return fetch(baseUrl + '/editors/indonesia', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+			credentials: 'include',
+            body: JSON.stringify(data)
+        });
+    }
+
+    function  uploadStudentList(data) {
+        return fetch(baseUrl + `/editors/indonesia/upload`, {
+            method: 'POST',
+			credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+    }
+
+    function CSVToArray(strData, strDelimiter) {
+		// Check to see if the delimiter is defined. If not,
+		// then default to comma.
+		strDelimiter = (strDelimiter || ",");
+		// Create a regular expression to parse the CSV values.
+		var objPattern = new RegExp((
+			// Delimiters.
+			"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+			// Quoted fields.
+			"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+			// Standard fields.
+			"([^\"\\" + strDelimiter + "\\r\\n]*))"), "gi");
+		// Create an array to hold our data. Give the array
+		// a default empty first row.
+		var arrData = [[]];
+		// Create an array to hold our individual pattern
+		// matching groups.
+		var arrMatches = null;
+		// Keep looping over the regular expression matches
+		// until we can no longer find a match.
+		while (arrMatches = objPattern.exec(strData)) {
+			// Get the delimiter that was found.
+			var strMatchedDelimiter = arrMatches[1];
+			// Check to see if the given delimiter has a length
+			// (is not the start of string) and if it matches
+			// field delimiter. If id does not, then we know
+			// that this delimiter is a row delimiter.
+			if (strMatchedDelimiter.length && (strMatchedDelimiter != strDelimiter)) {
+				// Since we have reached a new row of data,
+				// add an empty row to our data array.
+				arrData.push([]);
+			}
+			// Now that we have our delimiter out of the way,
+			// let's check to see which kind of value we
+			// captured (quoted or unquoted).
+			if (arrMatches[2]) {
+				// We found a quoted value. When we capture
+				// this value, unescape any double quotes.
+				var strMatchedValue = arrMatches[2].replace(
+					new RegExp("\"\"", "g"), "\"");
+			} else {
+				// We found a non-quoted value.
+				var strMatchedValue = arrMatches[3];
+			}
+			// Now that we have our value string, let's add
+			// it to the data array.
+			arrData[arrData.length - 1].push(strMatchedValue);
+		}
+		// Return the parsed data.
+		return (arrData);
+	}
+
     return {
         login,
         logout,
@@ -166,7 +254,12 @@ var User = (function () {
         update,
         queryBySingleKeyword,
         queryByExcel,
-        getLastTime
+        getLastTime,
+        getStudentList,
+        getStudentInfo,
+        saveStudentInfo,
+        CSVToArray,
+        uploadStudentList
         //getLoginUserInfo
     }
 })();
